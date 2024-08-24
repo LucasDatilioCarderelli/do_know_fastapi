d839bf99f07ef26124a0b19beb7587a59619a4e2
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2024-08-24 18:56:45 -0300

    add: aula 02

--- do_know_fastapi/app.py
+++ do_know_fastapi/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from do_know_fastapi.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_class=JSONResponse,
+    response_model=Message,
+)
 def read_root():
-    return {'message': 'Olar World'}
+    return {'message': 'Olar World', 'extra': 'extra'}
