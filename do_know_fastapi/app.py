b222785e2db397067921ed5b1e56ed4d6b007052
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2024-08-24 21:02:37 -0300

    add: aula03

--- do_know_fastapi/app.py
+++ do_know_fastapi/app.py
@@ -1,11 +1,19 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
-from do_know_fastapi.schemas import Message
+from do_know_fastapi.schemas import (
+    ListUserPublicSchema,
+    Message,
+    UserDBSchema,
+    UserPublicSchema,
+    UserSchema,
+)
 
 app = FastAPI()
+
+database = []
 
 
 @app.get(
@@ -16,3 +24,52 @@ app = FastAPI()
 )
 def read_root():
     return {'message': 'Olar World', 'extra': 'extra'}
+
+
+@app.post(
+    '/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema
+)
+def create_user(user: UserSchema):
+    user_with_id = UserDBSchema(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get(
+    '/users/',
+    status_code=HTTPStatus.OK,
+    response_model=ListUserPublicSchema,
+)
+def read_user():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublicSchema,
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 0 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDBSchema(id=user_id, **user.model_dump())
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=Message,
+)
+def delete_user(user_id: int):
+    if user_id < 0 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    database.pop(user_id - 1)
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,79 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from do_know_fastapi.app import app
-
-
-def test_app():
-    client = TestClient(app)
-
+def test_app(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olar World'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'user',
+            'email': 'email@email.com',
+            'password': 'password',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+
+
+def test_read_user(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'user',
+                'email': 'email@email.com',
+            }
+        ]
+    }
+
+
+def test_update_client(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'username',
+            'email': 'email@email.com',
+            'password': 'password',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'username',
+        'email': 'email@email.com',
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'user2',
+            'email': 'user2@email.com',
+            'password': 'password2',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
